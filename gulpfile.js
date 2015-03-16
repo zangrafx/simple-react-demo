1dbd7d78f8f4004c9e7d2ec295d0d9c9eadde56e
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2015-03-16 20:21:04 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+"use strict";
+
+var gulp = require("gulp"),
+    path = require("path"),
+    pckg = require(path.join(__dirname, "package.json")),
+    querystring = require("querystring"),
+    webpack = require("webpack");
+
+function webpackTask(isDebugMode, done) {
+    var env,
+        plugins;
+
+    if (isDebugMode) {
+        env = "development";
+    } else {
+        env = "production";
+    }
+
+    plugins = [
+        new webpack.DefinePlugin({
+            "process.env": {
+                "NODE_ENV": JSON.stringify(env)
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin(pckg.name + ".common.min.js")
+    ];
+
+    if (!isDebugMode) {
+        plugins.push(new webpack.optimize.UglifyJsPlugin());
+    }
+
+    webpack({
+        "bail": true,
+        "context": path.join(__dirname, "web_modules", pckg.name),
+        "debug": isDebugMode,
+        "devtool": "source-map",
+        "entry": {
+            "index": "./index"
+        },
+        "module": {
+            "loaders": [
+                {
+                    "test": /\.jsx$/,
+                    "exclude": /(bower_components|node_modules)/,
+                    "loader": "babel-loader?" + querystring.stringify({
+                        "loose": [
+                            "es6.modules",
+                            "es6.properties.computed",
+                            "es6.templateLiterals"
+                        ],
+                        "optional": [
+                            "runtime",
+                            "utility.deadCodeElimination",
+                            "utility.inlineExpressions",
+                            "validation.undeclaredVariableCheck",
+                            "validation.react"
+                        ]
+                    })
+                }
+            ]
+        },
+        "output": {
+            "filename": pckg.name + ".[name].min.js",
+            "path": __dirname + "/dist"
+        },
+        "plugins": plugins,
+        "resolve": {
+            "extensions": [
+                "",
+                ".coffee",
+                ".js",
+                ".jsx"
+            ]
+        }
+    }, done);
+}
+
+gulp.task("watch", function () {
+    gulp.watch(path.join(__dirname, "web_modules", "**", "*.jsx"), [
+        "webpack"
+    ]);
+});
+
+gulp.task("webpack", function (done) {
+    webpackTask(true, done);
+});
+
+gulp.task("webpack:deploy", function (done) {
+    webpackTask(false, done);
+});
+
+gulp.task("build", ["webpack"]);
+gulp.task("default", ["build"]);
+gulp.task("deploy", ["webpack:deploy"]);
---
+++ web_modules/react-webpack-boilerplate/index.jsx
@@ -0,0 +1,5 @@
+"use strict";
+
+import HelloWorld from "react-webpack-boilerplate/React/HelloWorld";
+
+React.render(<HelloWorld />, document.body);
